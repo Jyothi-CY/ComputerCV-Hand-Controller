042f23a3f8b29b96e9fb66584b1a636f9859a71d
Jyothirmayee Nisamkarao <157049957+Jyothi-CY@users.noreply.github.com>
2024-12-03 22:21:02 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menuBtnLinks = document.querySelector('.dropdown-menu a');
-const dropDownMenu = document.querySelector('.dropdown-menu');
-
-menuBtn.onclick = function () {
-  dropDownMenu.classList.toggle('open');
-}
